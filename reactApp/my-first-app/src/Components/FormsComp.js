157812657994b539ea2841d5bb76ac26bbadff9f
Asante-Adarkwa-Usman <asante.au96@gmail.com>
2019-09-17 13:11:42 -0400

    form component added

--- reactApp/my-first-app/src/Components/FormsComp.js
+++ reactApp/my-first-app/src/Components/FormsComp.js
@@ -10,29 +10,43 @@ import React, { Component } from 'react'
          super(props)
      
          this.state = {
-              name: ' '
+              Name : ' ',
+              Password : ' ',
+              ConfirmPassword : ''
          }
      }
 
      HandlesNameChange = event => {
          this.setState({
-             name: event.target.value
+             Name: event.target.value
          })
      }
      
+     HandlesPasswordChange = event => {
+        this.setState({
+            Password: event.target.value
+        })
+    }
+    HandlesConfirmPasswordChange = event => {
+        this.setState({
+            ConfirmPassword: event.target.value
+        })
+    }
      
     render() {
         return (
        <form>
            <div>
+           
                <label style={this.formDesign}> Name </label>
                <input type='text'  value = {this.state.name} onChange={this.HandlesNameChange}></input>
 
                <label style={this.formDesign}> Password </label>
-               <input type='text'  value = {this.state.name} onChange={this.HandlesNameChange}></input>
+               <input type='text'  value = {this.state.Password} onChange={this.HandlesPasswordChange}></input>
 
                <label style={this.formDesign}> Confirm Password </label>
-               <input type='text'  value = {this.state.name} onChange={this.HandlesNameChange}></input>
+               <input type='text'  value = {this.state.ConfirmPassword} onChange={this.HandlesConfirmPasswordChange}></input>
+               
            </div>
        </form>
         )
